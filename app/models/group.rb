17dec5942811b6716c12ef048cb09c586c757db4
Dimetriu <dima.rashidov@gmail.com>
2018-09-03 17:32:58 +0300

    improve Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
   has_many :memberships, inverse_of: :group
-  has_many :groups, through: :memberships, inverse_of: :groups
+  has_many :users, through: :memberships, inverse_of: :groups
 end
